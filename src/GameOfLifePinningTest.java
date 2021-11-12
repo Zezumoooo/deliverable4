457dff1ebf6c62ebd42198f649abcda87cd494ae
Zezumoooo <63968507+Zezumoooo@users.noreply.github.com>
2021-11-11 21:54:41 -0500

    Update GameOfLifePinningTest.java

--- src/GameOfLifePinningTest.java
+++ src/GameOfLifePinningTest.java
@@ -26,7 +26,6 @@ public class GameOfLifePinningTest {
 }
 @Test 
 	public void test1() {
-	Cell[][] boardCells=new Cell[5][5];
 	panel.calculateNextIteration();
 	for(int i=0;i<2;i++) {
 		for(int j=0;j<5;j++) {
@@ -58,7 +57,6 @@ public class GameOfLifePinningTest {
 }
 @Test 
 	public void test2() {
-	Cell[][] boardCells=new Cell[5][5];
 	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j < 5; j++) {
 			if (i==2&&j==1) {
