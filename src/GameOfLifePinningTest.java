bf34bb11eb8d270af68d7e145abe798b2926bcbe
Zezumoooo <63968507+Zezumoooo@users.noreply.github.com>
2021-11-11 23:29:54 -0500

    Update GameOfLifePinningTest.java

--- src/GameOfLifePinningTest.java
+++ src/GameOfLifePinningTest.java
@@ -41,13 +41,23 @@ public class GameOfLifePinningTest {
 }
 @Test 
 	public void test2() {
-	Boolean result=panel.iterateCell(x:2,y:2);
-	for (int i = 1; i < 4; i++) {
-		for (int j = 1; j <4; j++) {
-			Mockito.verify(boardCells[i][j].getAlive());
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (j==2) {
+				if (i == 1 || i == 2 || i == 3) {
+					assertTrue(panel.iterateCell(i,j));
+					break;
+				}
+				else {
+					assertFalse(panel.iterateCell(i,j));
+					break;
+				}
+			}
+			else {
+				assertFalse(panel.iterateCell(i,j));
+			}
 		}
 	}
-	assertEquals(expected: true,result);
 }
 @Test 
 	public void test3() {
